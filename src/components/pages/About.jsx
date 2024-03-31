abbf382a0997f581a88191e29fac18ae65110751
Russel <abdurrahmanrussel77@gmail.com>
2024-04-01 00:24:04 +0600

    Feature added to About US

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,20 @@
-
+import React from "react";
 
 export const About = () => {
-    return <h1>Hello! About Page</h1>
+    return ( 
+    <> 
+      <div className="about-us">
+      <h1>About Us</h1>
+      <p>
+        We have created this website for learning programming. Our goal is to discuss fundamental concepts of programming, topics, and their solutions. 
+        We explore ways to learn programming in Bangla and strive to connect with our community.
+      </p>
+      <p>
+        For any questions, comments, or suggestions, please  <a href="/contact">contact us</a>.
+      </p>
+    </div>
+
+
+    </>
+    )
 };
